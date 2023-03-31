a62bac5844d34aaa4ef40fb70ef2e0898720c42e
Victor <victorribeiro172@gmail.com>
2023-03-31 14:58:36 -0300

    criação de funções para exibição

--- readInput.py
+++ readInput.py
@@ -8,6 +8,10 @@ def readInput(filename):
         data = file.readlines()
     return data
 
+def escreveSaida(dados, filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(dados)
+
 def inputHandling(data):
     for line in range(len(data)):
         data[line] = data[line].strip().replace(' ', '')
@@ -24,6 +28,19 @@ def makeDictionary(cleanData):
         else:
             return False
     return dictionary
+
+def exibeAP(Q0, Q1, QF):
+    dadosAp = ''
+    dadosAp += 'Estado (q0)\n'
+    dadosAp +='Transições: \n'
+    dadosAp += Q0.exibeEstado()+'\n'
+    dadosAp += 'Estado (q1)\n'
+    dadosAp +='Transições: \n'
+    dadosAp += Q1.exibeEstado()+'\n'
+    dadosAp += 'Estado (qf)\n'
+    dadosAp +='Estado Final!\n'
+    dadosAp += QF.exibeEstado()
+    return dadosAp
 
 def mainInputReady():
     cleanInput = inputHandling(readInput('input.txt'))
